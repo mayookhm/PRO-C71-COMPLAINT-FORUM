78d50e192c20341891351b7179e5990bb4da0d1e
mayookhm <79088427+mayookhm@users.noreply.github.com>
2021-12-09 17:13:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,14 @@ require("@firebase/firestore")
 
 const firebaseConfig = {
  //Add configuration here
- 
+ const firebaseConfig = {
+  apiKey: "AIzaSyA9-H8Q9UY3TosQnKh3F_IPUw02Lsndkk8",
+  authDomain: "wily-app-19511.firebaseapp.com",
+  projectId: "wily-app-19511",
+  storageBucket: "wily-app-19511.appspot.com",
+  messagingSenderId: "143796590968",
+  appId: "1:143796590968:web:a6be301f0b823e3ea2908d"
+};
 };
   firebase.initializeApp(firebaseConfig);
 
